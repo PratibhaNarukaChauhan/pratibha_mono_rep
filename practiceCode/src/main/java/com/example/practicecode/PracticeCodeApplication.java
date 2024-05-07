b1102e18095aaf2d5dd01532dbbbf1bfc33ce2c8
pratibha chauhan <pratibhanaruka8@gmail.com>
2024-05-07 14:28:01 +0530

    run and tested a maven project.closes #7

--- practiceCode/src/main/java/com/example/practicecode/PracticeCodeApplication.java
+++ practiceCode/src/main/java/com/example/practicecode/PracticeCodeApplication.java
@@ -2,12 +2,19 @@ package com.example.practicecode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class PracticeCodeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PracticeCodeApplication.class, args);
     }
-
+    @GetMapping("/")
+    public String getMessage()
+    {
+        return "Hello";
+    }
 }
