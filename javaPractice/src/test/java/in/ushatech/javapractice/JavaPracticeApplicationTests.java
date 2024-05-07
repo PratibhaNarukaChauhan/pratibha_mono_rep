88bd3f22685314f49b89995b6be19478035a7561
pratibha chauhan <pratibhanaruka8@gmail.com>
2024-05-07 14:28:01 +0530

    run and tested lablled for loop.closes #8

---
+++ javaPractice/src/test/java/in/ushatech/javapractice/JavaPracticeApplicationTests.java
@@ -0,0 +1,15 @@
+package in.ushatech.javapractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPracticeApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
--- practiceCode/src/main/java/com/example/practicecode/PracticeCodeApplication.java
+++ practiceCode/src/main/java/com/example/practicecode/PracticeCodeApplication.java
@@ -2,12 +2,15 @@ package com.example.practicecode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+
 public class PracticeCodeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PracticeCodeApplication.class, args);
-    }
 
+    }
 }
